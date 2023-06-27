60a5eb1ca00e3c43a400c6445ad9cdea6e101ed4
SandhyaAchu <sandhyaek11@gmail.com>
2023-06-27 19:35:53 +0530

    firstcommit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from "./sandhya"
+
+function App() {
+  return (
+    <div>
+     
+      <h1>Sandhya</h1>
+    </div>
+  );
+}
+
+export default App;
